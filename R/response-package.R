db116ed43a7ba9f57649a3c0a12325de7c276071
James Edwards <jamese1326@gmail.com>
2022-01-07 11:59:54 +0000

    Add docs, license and dependencies
    Passing R cmd check.

--- R/response-package.R
+++ R/response-package.R
@@ -1,6 +1,8 @@
 #' @keywords internal
 #' @importFrom ggplot2 aes
-
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data :=
+#' @aliases response-package
 "_PACKAGE"
 
 ## usethis namespace: start
--- R/response.R
+++ R/response.R
@@ -1,20 +1,16 @@
 #' Confidence intervals for binary target variable by values of a discrete predictor
 #'
-#' For a data frame input, one variable is a binary target (`target_name`) and another is selected to
-#' be a predictor variable (`var_name`). Mean response and a confidence interval is calculated for the
-#' target variable for each level or value of the predictor. The results are plotted and returned as a
-#' table. The function most appropriate for factor predictors but will work with other variable types also.
+#' In the `data`, one column is a response variable (`target_name`) and another is a predictor or
+#' grouping variable (`var_name`). A mean and confidence interval of the response for each value
+#' of the predictor is calculated and returned in a data frame. The function most appropriate for
+#' factor predictors where grouping is more natural but will also work with other variable types.
 #'
-#' The target variable must be binary. Top compute confidence intervals this is converted to 0 and 1 values.
-#' If it is not obvious which value corresponds to 1 and which to 0 then it will be based on level order
-#' if a factor and the first observation otherwise. Giving the value of corresponding to 1 in the argument
-#' `pos_class` will override this.
+#' The target variable must be binary. To compute confidence intervals, values are treated as 0
+#' and 1 values. Which value corresponds to 1 (the "positive class") is controlled by the argument
+#' `pos_class`. If not supplied, the positive class will be the "greatest" value using natural
+#' ordering (level ordering if target is an ordered factor).
 #'
-#' Use the `plot` and `return_plot` arguments to control output. By default (designed to be
-#'  used interactively) returns a table and prints a plot.  If `return_plot = TRUE` then just the
-#'  plot is returned. If  `return_plot = FALSE` and
-#'  `plot = FALSE` then the table is returned and no plot is generated. The default
-#'
+#' @return A summary data frame. By default a plot will also be printed as a side effect.
 #' @param dt A data frame.
 #' @param target_name Column to use as target variable. Column name (quoted or unquoted) or position.
 #' @param var_name Column to use as predictor variable. Column name (quoted or unquoted) or position.
@@ -26,7 +22,7 @@
 #'   and orders by number of observations otherwise.
 #' @param conf_level Numeric in (0,1). Confidence level used for confidence intervals.
 #' @param pos_class Optional. Specify value in target to associate with class 1.
-#' @param plot Optional logical. Output a plot or not.
+#' @param plot Logical. If `TRUE`, a plot using `plot_reponse()` will be printed.
 #' @param family Distribution family to use for the response confidence interval. Either "binomial"
 #'   or "gaussian".
 #'
@@ -49,7 +45,7 @@ response <- function(dt, target_name, var_name, min_n = 1, show_all = TRUE, orde
 
   if (family == "gaussian"){
     if (!(is.numeric(dt$target) | is.logical(dt$target))) {
-      stop("Target variable must be numeric or logical", call. = FALSE)
+      stop('Target variable must be numeric or logical with `family = "gaussian"`.', call. = FALSE)
     }
   }
 
@@ -76,7 +72,7 @@ response <- function(dt, target_name, var_name, min_n = 1, show_all = TRUE, orde
                      n_pos = sum(.data$target),
                      prop = mean(.data$target),
                      mean_response = mean(.data$target),
-                     sd = sd(.data$target)) %>%
+                     sd = stats::sd(.data$target)) %>%
     dplyr::rename(value = .data$var) %>%
     dplyr::filter(.data$n >= min_n)
 
@@ -111,19 +107,58 @@ response <- function(dt, target_name, var_name, min_n = 1, show_all = TRUE, orde
   res
 }
 
+#' Plot output from `response()`
+#'
+#' A mean and confidence interval is plotted by group. The axes are flipped so that
+#' groups are displayed on the y-axis, with response on the x-axis. A vertical dashed
+#' line shows the overall mean of the data. Colours indicate whether the confidence interval
+#' crosses the global mean (blue), or is entirely above it (green) or below it (red).
+#'
+#' @param data Data frame. Output from `response()`.
+#' @param order_n Logical. If `TRUE`, groups are ordered by number of observations.
+#' @param response_lim Optional x axis limits passed to `ggplot()` e.g. `c(0,1)`.
+#' @export
+plot_response <- function(data, order_n = FALSE, response_lim  = NULL) {
+  mean_all <- attr(data, "mean_all")
+  if (order_n){
+    data <- dplyr::mutate(data, value = stats::reorder(.data$value, .data$n))
+  }
+  cols <- c("#F8766D", "#00BA38", "#619CFF")
+  gg <- data %>%
+    ggplot2::ggplot(aes(x = .data$value, y = .data$prop, color = .data$sig)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_errorbar(aes(ymin = .data$lo, ymax = .data$hi)) +
+    ggplot2::coord_flip() +
+    ggplot2::geom_hline(yintercept = mean_all, linetype = 2) +
+    ggplot2::ylab("Mean Response") +
+    ggplot2::xlab(attr(data, "target_name")) +
+    ggplot2::theme(legend.position = "none") +
+    ggplot2::scale_colour_manual(values = c("lo" = cols[1], "none" = cols[3], "hi" = cols[2])) +
+    {if(all(!is.null(response_lim))) ggplot2::ylim(response_lim[1], response_lim[2])}
+  gg
+}
+
+#' Prepare response vector when family is binomial
+#'
+#' Converts input to a logical vector with attribute recording which
+#' values in the original vector correspond to positive and negative
+#' classes (respectively `TRUE` and `FALSE` in the output).
+#'
+#' Used in `response()`.
+#'
 #' @param x Target variable vector.
 #' @param pos_class A value in `x` to use as the positive class.
-#' @keywords internal
+#' @noRd
 prepare_binomial_response <- function(x, pos_class = NULL) {
-  if (length(na.omit(unique(x))) != 2L){
+  if (length(stats::na.omit(unique(x))) != 2L){
     stop("Target variable must be binary.", call. = FALSE)
   }
   if (!is.null(pos_class)){
     if (!is.atomic(pos_class) || !length(pos_class) == 1L){
-      stop("`pos_class` must be length 1 and atomic.", .call = FALSE)
+      stop("`pos_class` must be length 1 and atomic.", call. = FALSE)
     }
     if (!pos_class %in% x){
-      stop("`pos_class` must match a value in the target vector.", .call = FALSE)
+      stop("`pos_class` must match a value in the target vector.", call. = FALSE)
     }
   }
   if (is.null(pos_class)){
@@ -135,8 +170,10 @@ prepare_binomial_response <- function(x, pos_class = NULL) {
   x
 }
 
+#' Message text when a positive class is not supplied for a binomial response
+#'
 #' @param classes Length two vector with positive class first.
-#' @keywords internal
+#' @noRd
 pos_class_message <- function(classes) {
   paste0("Argument `pos_class` not supplied. Treating:\n",
   "  * ", glue::double_quote(classes[1]), " as positive class (1).\n",
@@ -144,48 +181,49 @@ pos_class_message <- function(classes) {
   "Use `pos_class` argument to set different value for positive class.")
 }
 
-#' @param prop_lim Optional x axis limits passed to `ggplot()` e.g. `c(0,1)`.
+#' Add confidence interval columns to data
 #'
-plot_response <- function(dt, order_n = FALSE, prop_lim  = NULL) {
-  mean_all <- attr(dt, "mean_all")
-  if (order_n){
-    reponse_dt <- dplyr::mutate(dt, value = stats::reorder(.data$value, .data$n))
-  }
-  cols <- c("#F8766D", "#00BA38", "#619CFF")
-  gg <- dt %>%
-    ggplot2::ggplot(aes(x = .data$value, y = .data$prop, color = .data$sig)) +
-    ggplot2::geom_point() +
-    ggplot2::geom_errorbar(aes(ymin = .data$lo, ymax = .data$hi)) +
-    ggplot2::coord_flip() +
-    ggplot2::geom_hline(yintercept = mean_all, linetype = 2) +
-    ggplot2::ylab("Mean Proportion Target") +
-    ggplot2::xlab(attr(dt, "target_name")) +
-    ggplot2::theme(legend.position = "none") +
-    ggplot2::scale_colour_manual(values = c("lo" = cols[1], "none" = cols[3], "hi" = cols[2])) +
-    {if(all(!is.null(prop_lim))) ggplot2::ylim(prop_lim[1], prop_lim[2])}
-  gg
-}
-
-ci_add <- function(dt, conf_level, family) {
+#' Called from `response()`. Currently a Gaussian CI is used unless `family = "binomial"`.
+#' Input `data` has columns `n`. If family is binomial then also has `prop` column. For
+#' Gaussian CI it will have columns `mean_response` and `sd`.
+#' @param data Data frame.
+#' @param conf_level Numeric scalar in (0, 1). Confidence level for the confidence interval.
+#' @param family String.
+#' @noRd
+ci_add <- function(data, conf_level, family) {
   if (family == "binomial"){
-    binom <- binom::binom.wilson(dt$prop * dt$n, dt$n, conf.level = conf_level)
-    return(dplyr::mutate(dt, lo = binom[["lower"]], hi = binom[["upper"]]))
+    binom <- binom::binom.wilson(data$prop * data$n, data$n, conf.level = conf_level)
+    return(dplyr::mutate(data, lo = binom[["lower"]], hi = binom[["upper"]]))
   }
-  dplyr::mutate(dt,
+  dplyr::mutate(data,
                   lo = ci_t(.data$mean_response, .data$sd, .data$n, ci = conf_level, lower = TRUE),
                   hi = ci_t(.data$mean_response, .data$sd, .data$n, ci = conf_level, lower = FALSE))
 }
 
-# t-distribution based confidence interval using summary statistics as input.
+#' t-distribution based confidence interval using summary statistics as input
+#'
+#' @param mean Mean.
+#' @param sd Standard deviation.
+#' @param n Number of observations.
+#' @param ci Confidence level to use.
+#' @param lower Return lower end of CI if `TRUE`, upper otherwise.
+#' @noRd
 ci_t <- function (mean, sd, n, ci = 0.95, lower = TRUE){
   quan <- ci + (1 - ci)/2
-  margin_error <- qt(quan, df = n - 1) * sd / sqrt(n)
+  margin_error <- stats::qt(quan, df = n - 1) * sd / sqrt(n)
   if (lower) return(mean - margin_error)
   mean + margin_error
 }
 
+#' Choose response distribution family based on response values
+#'
+#' Returns `binomial` if `x` has up to two unique, non-missing values.
+#' Otherwise return `gaussian` if `x` is numeric and error if neither of these
+#' conditions is met.
+#' @param x Response vector
+#' @noRd
 guess_family <- function(x) {
-  if (length(na.omit(unique(x))) == 2L){
+  if (length(stats::na.omit(unique(x))) <= 2L){
     return("binomial")
   }
   if (is.numeric(x)){
